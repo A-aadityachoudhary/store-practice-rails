d0c8921d4c9dcbdb7b7dcf5df189e8a82ce5314a
Aaditya Choudhary <aadityach812@gmail.com>
2026-04-09 14:20:59 +0530

    first commit

---
+++ app/models/product.rb
@@ -0,0 +1,11 @@
+class Product < ApplicationRecord
+    include Notifications
+    has_many :subscribers, dependent: :destroy
+    has_rich_text :description
+    has_one_attached :featured_image
+    validates :name, presence:true
+    validates :inventory_count, numericality: {greater_than_or_equal_to: 0}
+    
+  
+    
+end
---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  get  "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  get  "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  resources :sessions, only: [:index, :show, :destroy]
+  resource  :password, only: [:edit, :update]
+
+  namespace :identity do
+    resource :email,              only: [:edit, :update]
+    resource :email_verification, only: [:show, :create]
+    resource :password_reset,     only: [:new, :edit, :create, :update]
+  end
+
+  root "products#index"
+
+  resources :products do
+    resources :subscribers, only: [:create]
+  end
+
+  resource :unsubscribe, only: [:show]
+end
